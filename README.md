ed39ecceeddd855c20bff8d91eced0d4484f7f12
HaoJu Zheng <zhenghaoju700@163.com>
2015-06-04 14:09:31 +0800

    update README.md and add params

--- README.md
+++ README.md
@@ -6,7 +6,16 @@ Just a html2js tool for cuf team
 - Put your css file into src/css dir
 - ```npm install -g gulp```
 - ```npm install```
-- Run command ```FILE_NAME=xxx.js gulp```
+- Run command ```gulp```
+
+Params
+
+```
+--concat-file-name ${{fileName}}
+--uglify
+--cssmini
+--template-module-name ${{moduleName}}
+```
 
 [gulp相关资料](https://github.com/hjzheng/CUF_meeting_knowledge_share/issues/33)
 
--- gulpfile.js
+++ gulpfile.js
@@ -1,46 +1,57 @@
 var gulp = require('gulp');
-var ngHtml2Js = require("gulp-ng-html2js");
-var concat = require("gulp-concat");
-var clean = require("gulp-clean");
-var ngAnnotate = require('gulp-ng-annotate');
-var minifyCss = require('gulp-minify-css');
-var gulpif = require('gulp-if');
-var uglify = require('gulp-uglify');
+//Use gulp-load-plugins to auto require plugins
+//The plugin name: transforms hyphenated plugins names to camel case
+var plugins = require('gulp-load-plugins')();
+/*
+ var ngHtml2Js = require("gulp-ng-html2js");
+ var concat = require("gulp-concat");
+ var clean = require("gulp-clean");
+ var ngAnnotate = require('gulp-ng-annotate');
+ var minifyCss = require('gulp-minify-css');
+ var gulpif = require('gulp-if');
+ var uglify = require('gulp-uglify');
+ */
+var _ = plugins.loadUtils(['log', 'colors', 'env']);
 var eventStream = require('event-stream');
 
-//read env variable, you can use command "FILE_NAME=test.js gulp"
-var _fileName = process.env.FILE_NAME || "all.js";
-var _uglify = false;
-var _cssmini = false;
-var _moduleName = "cuf.template";
+//read env variable
+var _fileName = _.env['concat-file-name'] || "all.js";
+var _uglify = _.env.uglify || false;
+var _cssmini = _.env.cssmini || false;
+var _moduleName = _.env['template-module-name'] || "cuf.template";
+/*
+ --file-name ${{fileName}}
+ --uglify
+ --cssmini
+ --module-name ${{moduleName}}
+ */
 
 gulp.task('buildcss', ['clean'], function(){
-   return gulp.src('./src/css/*.css')
-    .pipe(gulpif(_cssmini, minifyCss()))
-    .pipe(gulp.dest("./dist/css"));
+    return gulp.src('./src/css/*.css')
+        .pipe(plugins.if(_cssmini, plugins.minifyCss()))
+        .pipe(gulp.dest("./dist/css"));
 });
 
 gulp.task('clean', function(){
-   return gulp.src('./dist')
-    .pipe(clean({force: true}));
+    return gulp.src('./dist')
+        .pipe(plugins.clean({force: true}));
 });
 
 gulp.task('buildjs', ['clean'], function() {
-   
-   var jsStream = gulp.src("./src/js/*.js"),
-       htmlStream = gulp.src("./src/template/*.html");
 
-   htmlStream.pipe(ngHtml2Js({
+    var jsStream = gulp.src("./src/js/*.js"),
+        htmlStream = gulp.src("./src/template/*.html");
+
+    htmlStream.pipe(plugins.ngHtml2js({
         moduleName: _moduleName,
         prefix: "template/"
-   }));
+    }));
 
-   return eventStream.merge(jsStream, htmlStream)
-    .pipe(concat(_fileName))
-    .pipe(ngAnnotate())
-    .pipe(gulpif(_uglify, uglify()))
-    .pipe(gulp.dest("./dist/js"))
+    return eventStream.merge(jsStream, htmlStream)
+        .pipe(plugins.concat(_fileName))
+        .pipe(plugins.ngAnnotate())
+        .pipe(plugins.if(_uglify, plugins.uglify()))
+        .pipe(gulp.dest("./dist/js"))
 });
 
 gulp.task('default', ['clean', 'buildjs', 'buildcss']);
-
